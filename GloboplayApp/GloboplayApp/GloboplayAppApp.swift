6f319b2bec46ea8d831dd9aa48807477d58b0e38
Gabriel Batista <gabriel.devproject@gmail.com>
2022-10-31 11:38:40 -0300

    Add xcodeproject

---
+++ GloboplayApp/GloboplayApp/GloboplayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GloboplayAppApp.swift
+//  GloboplayApp
+//
+//  Created by Gabriel Batista on 31/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GloboplayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
